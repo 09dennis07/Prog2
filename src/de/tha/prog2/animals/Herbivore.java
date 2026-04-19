ed765795b71f2435323bd7f0eda02c03684bfd3f
denni <denni@Dennis-PC.fritz.box>
2026-04-19 12:52:06 +0200

    Sheet 03 Lösung

---
+++ src/de/tha/prog2/animals/Herbivore.java
@@ -0,0 +1,7 @@
+package de.tha.prog2.animals;
+
+public interface Herbivore extends Animal{
+	
+	public void eatPlant();
+
+}
---
+++ src/de/tha/prog2/exceptions/HabitatFullException.java
@@ -0,0 +1,11 @@
+package de.tha.prog2.exceptions;
+
+public class HabitatFullException extends Exception{
+	
+	public HabitatFullException(String message) {
+		super(message);
+	}
+	
+	public HabitatFullException() {}
+
+}
