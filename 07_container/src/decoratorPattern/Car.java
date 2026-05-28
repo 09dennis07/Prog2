7ae1ec550fdfc26d7eddaa50c22733e3fb1a44a1
Dennis Florian Sandvoß <56682953+09dennis07@users.noreply.github.com>
2026-05-28 16:08:49 +0200

    Delete 07_container directory

--- 07_container/src/decoratorPattern/Car.java
+++
@@ -1,9 +0,0 @@
-package decoratorPattern;
-
-public interface Car {
-    void increaseSpeed();
-    void decreaseSpeed();
-    void turnLeft();
-    void turnRight();
-    void printSpeed();
-}
--- 07_container/src/vector1/shapes/Drawable.java
+++
@@ -1,9 +0,0 @@
-package vector1.shapes;
-
-import java.awt.Graphics;
-import java.awt.Color;
-public interface Drawable {
-  Color defaultColor=Color.blue;
-  void setColor(Color c);
-  void draw(Graphics g);
-}
